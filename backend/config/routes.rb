ddbff93a0ff0e6b6303457ee6fd31c8b33e4b6b0
joaopfsilva <j0a0.silva.kle@gmail.com>
2021-11-01 20:54:38 +0100

    added serializer

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :cars, only: %i(index show)
+
+      post "/login", to: "auth#login"
+      get "/auth", to: "auth#persist"
+    end
+  end
 end
