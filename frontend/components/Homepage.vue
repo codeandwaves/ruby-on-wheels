b9b0640fb4ffd2ddc320915cac4f660e19bc5622
joaopfsilva <j0a0.silva.kle@gmail.com>
2021-11-01 21:30:29 +0100

    setup nuxt and axios

---
+++ frontend/components/Homepage.vue
@@ -0,0 +1,20 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <h1>Cars</h1>
+    <ul v-for="car in cars" :key="car.id">
+      <li>{{ car.name }}</li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const cars = await $axios.$get("api/v1/cars");
+    console.log(cars);
+    return { cars };
+  },
+};
+</script>
