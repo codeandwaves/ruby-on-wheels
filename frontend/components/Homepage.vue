f7e582c95d3fb57664abe7165726fe947617bbf6
joaopfsilva <j0a0.silva.kle@gmail.com>
2021-11-01 22:15:13 +0100

    add nuxt project and axios library. WIP: tempalte homepage

---
+++ frontend/components/Homepage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Cars</h1>
+    <ul v-for="car in cars" :key="car.id">
+      <li>{{ car.name }}</li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const cars = await $axios.$get("api/v1/cars");
+    console.log(cars);
+    return { cars };
+  },
+};
+</script>
