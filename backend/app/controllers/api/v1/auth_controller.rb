2151a25e843e070c281a026dbe7e299cd6c71a92
João Silva <j0a0.silva.kle@gmail.com>
2021-11-02 20:54:32 +0100

    bugfix reading JWT token (#13)

--- backend/app/controllers/api/v1/auth_controller.rb
+++ backend/app/controllers/api/v1/auth_controller.rb
@@ -12,17 +12,16 @@ class Api::V1::AuthController < ApplicationController
   end
 
   def persist
-    user = User.last
-    render json: { "user" => user.to_json }
-    # if request.headers['Authorization']
-    #   encoded_token = request.headers['Authorization'].split(' ')[1]
-    #   token = JWT.decode(encoded_token, Rails.application.secrets.secret_key_base)
-    #   jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
+    # user = User.last
+    # render json: { "user" => user.to_json }
+    if request.headers['Authorization']
+      token = request.headers['Authorization'].split(" ")[1]
+      jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
 
-    #   user_id = token[0]['user_id']
-    #   user = User.find(user_id)
-    #   render json: user
-    # end
+      user_id = jwt_payload['id']
+      user = User.find(user_id)
+      render json: { "user" => user.to_json }
+    end
   end
 
   private
